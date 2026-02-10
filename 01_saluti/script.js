bf2353d606e82ee8dcd60e0f24cc54a741be02b5
pietrobertino <pietrobertino20@gmail.com>
2026-02-10 16:19:05 +0100

    complete first exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+const userName = prompt("Inserisci il tuo nome");
 
 
 // Dichiara la funzione qui.
 
+const logGreeting = nome => {
+    let message = `Ciao ${nome}`;
+    return message;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(logGreeting(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
