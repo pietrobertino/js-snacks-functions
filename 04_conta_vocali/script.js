6519275f8f26e14a8b32ef8b14869ab7d3071878
pietrobertino <pietrobertino20@gmail.com>
2026-02-10 16:56:04 +0100

    complete fourth exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,25 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const countVowels = str => {
+    const vowels = ["a", "e", "i", "o", "u"];
+    str = str.toLowerCase();
+    result = 0;
+    for (const letter of str) {
+        for (const vowel of vowels) {
+            if (letter === vowel) {
+                result++; 
+                break;
+            }
+        }
+    }
+    return result;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vowel_number = countVowels(word);
+console.log(vowel_number);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
