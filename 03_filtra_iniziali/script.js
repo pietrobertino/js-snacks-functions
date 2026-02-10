f2d0baf26a8b1b2fb38fc75806ca71f3e7829011
pietrobertino <pietrobertino20@gmail.com>
2026-02-10 16:35:14 +0100

    complete third exercise

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getWordsByInitial(arr, letter) {
+    let wordsByInitial = [];
+    for (const word of arr) {
+        if (word[0] === letter) {
+            wordsByInitial.push(word);
+        }
+    }
+    return wordsByInitial;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const initial = "A";
+
+console.log(getWordsByInitial(names,initial));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
